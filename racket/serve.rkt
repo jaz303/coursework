774e36618a4ad8fc21fa63a31f8cd988faa6d2ee
Jason Frame <jason@onehackoranother.com>
2013-07-04 10:40:46 +0100

    server + continuations and idkwtf is going on

--- racket/serve.rkt
+++ racket/serve.rkt
@@ -1,5 +1,9 @@
 #lang racket
 
+(require xml
+         net/url
+         racket/control)
+
 (define (go)
   'yep-it-works)
 
@@ -16,6 +20,7 @@
 
 (define (accept-and-handle listener)
   (define cust (make-custodian))
+  (custodian-limit-memory cust (* 50 1024 1024))
   (parameterize ([current-custodian cust])
     (define-values (in out) (tcp-accept listener))
     (thread (lambda ()
@@ -28,11 +33,79 @@
             (custodian-shutdown-all cust))))
 
 (define (handle in out)
-  ; discard the request heaader
-  ; operates directly on input stream...
-  (regexp-match #rx"(\r\n|^)\r\n" in)
-  ; send reply
-  (display "HTTP/1.0 200 OK\r\n" out)
-  (display "Server: k\r\nContent-Type: text/html\r\n\r\n" out)
-  (display "<html><body>Hello world!</body><html>" out))
+  (define req
+    (regexp-match #rx"^GET (.+) HTTP/[0-9]+\\.[0-9]+"
+                  (read-line in)))
+  (when req
+    ; Discard the rest of the header (up to the blank line):
+    (regexp-match #rx"(\r\n|^)\r\n" in)
+    ; Dispatch:
+    (let ([xexpr (prompt (dispatch (list-ref req 1)))])
+      (display "HTTP/1.0 200 OK\r\n" out)
+      (display "Server: k\r\nContent-Type: text/html\r\n\r\n" out)
+      (display (xexpr->string xexpr) out))))
 
+(define (dispatch str-path)
+  (define url (string->url str-path))
+  (define path (map path/param-path (url-path url)))
+  (define h (hash-ref dispatch-table (car path) #f))
+  (if h
+      (h (url-query url))
+      `(html (head (title "Error"))
+             (body
+              (font ((color "red"))
+                    "Unknown page: "
+                    ,str-path)))))
+
+(define (build-request-page label next-url hidden)
+  `(html
+    (head (title "Enter a Number to Add"))
+    (body ([bgcolor "white"])
+          (form ([action ,next-url] [method "get"])
+                ,label
+                (input ([type "text"] [name "number"]
+                                      [value ""]))
+                (input ([type "hidden"] [name "hidden"]
+                                        [value ,hidden]))
+                (input ([type "submit"] [name "enter"]
+                                        [value "Enter"]))))))
+
+(define (many query)
+  (build-request-page "Number of greetings:" "/reply" ""))
+
+(define (reply query)
+  (define n (string->number (cdr (assq 'number query))))
+  `(html (body ,@(for/list ([i (in-range n)])
+                   " hello"))))
+
+
+(define dispatch-table (make-hash))
+
+(hash-set! dispatch-table "hello"
+           (lambda (query)
+             `(html (body "Hello, world!"))))
+
+(hash-set! dispatch-table "many" many)
+(hash-set! dispatch-table "reply" reply)
+
+; Continuations
+
+(define (send/suspend mk-page)
+  (let/cc k
+    (define tag (format "k~a" (current-inexact-milliseconds)))
+    (hash-set! dispatch-table tag k)
+    (abort (mk-page (string-append "/" tag)))))
+
+(define (get-number label)
+  (define query
+    (send/suspend
+     (lambda (k-url)
+       (build-request-page label k-url ""))))
+  (string->number (cdr (assq 'number query))))
+
+(define (sum2 query)
+  (define m (get-number "First number:"))
+  (define n (get-number "Second number:"))
+  `(html (body "The sum is " ,(number->string (+ m n)))))
+
+(hash-set! dispatch-table "sum2" sum2)
